9a2e0e2afacb8b05b0beb8abfa5136fb858d8a80
mohamedfahim1045 <mohamedfahim1045@gmail.com>
2024-10-21 15:50:51 +0300

    fix one error

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,21 +1,12 @@
 import Cookies from 'js-cookie';
 import { jwtDecode } from 'jwt-decode';
 import { createContext, ReactNode, useEffect, useState } from 'react';
+import { UserData } from '../utils/InterFaces';
 
-interface DecodedToken {
-    firstName?: string;
-    lastName?: string;
-    email?: string;
-    age?: number;
-    birthDate?: string;
-    image?: string;
-    phone?: string;
-    gender?: string;
-}
 
 interface AuthContextProps {
     token: string | null;
-    decodedToken: DecodedToken | null;
+    decodedToken: UserData | null;
 }
 
 export const AuthContext = createContext<AuthContextProps | null>(null);
@@ -25,14 +16,14 @@ interface AuthContextProviderProps {
 }
 
 export default function AuthContextProvider(props: AuthContextProviderProps) {
-    const [decodedToken, setDecodedToken] = useState<DecodedToken | null>(null);
+    const [decodedToken, setDecodedToken] = useState<UserData | null>(null);
     const [token, setToken] = useState<string | null>(null);
 
     const saveUserData = () => {
         const storedToken = Cookies.get('authToken');
         if (storedToken) {
             try {
-                const decoded = jwtDecode<DecodedToken>(storedToken);
+                const decoded = jwtDecode<UserData>(storedToken);
                 setDecodedToken(decoded);
                 setToken(storedToken);
             } catch (error) {
